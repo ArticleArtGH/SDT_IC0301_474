da8451924fa4a36337dc02f1d129c218eaea5649
Chernykh Artyom <artemchernykh07@gmail.com>
2020-10-29 20:45:57 +0300

    not lucky

---
+++ Practice_work_6/lab-06.lua
@@ -0,0 +1,43 @@
+#!/usr/bin/lua5.3
+
+lgi = require 'lgi'
+--package.loaded[lsqlite3]
+sqlite = require 'lsqlite3'
+
+gtk = lgi.Gtk
+pb = lgi.GdkPixbuf.Pixbuf
+
+gtk.init()
+
+bld = Builder()
+bld:add_from_file('lab-06.glade')
+
+ui = bld.objects
+
+rdr_txt = gtk.CellRendererText{}
+rdr_pb = gtk.CellRendererPixbuf{}
+
+c1 = gtk.TreeViewColumn { title = 'Name', { rdr_txt, { text = 1 }} }
+c2 = gtk.TreeViewColumn { title = 'Value', { rdr_txt, { text = 2 }} }
+c3 = gtk.TreeViewColumn { title = 'Image', { rdr_pb, { pb = 3 }} }
+
+ui.tree:append_column(c1)
+ui.tree:append_column(c2)
+ui.tree:append_column(c3)
+
+--[[db = sqlite.open('lab-06.db')
+
+for row in db:rows('select * from list') do
+local pb = pixbuf.new_from_file(row.image)
+
+el = ui.tree_mdl:append()
+ui.tree_mdl[el] = { [1] = row.name, [2] = row.value, [3] = pb }
+end
+
+db.close()]]
+
+ui.wnd.title = 'lab-06'
+ui.wnd.on_destroy = gtk.main_quit
+ui.wnd:show_all()
+
+gtk.main()
---
+++ Practice_work_6/lab-06.sql
@@ -0,0 +1,12 @@
+drop table if exists list;
+create table list {name text, value int, image text};
+
+insert into list values {'Apple'	,10	,'img/img-apple.png'};
+insert into list values {'Blueberry'	,100	,'img/img-bluberry.png'};
+insert into list values {'Cherry'	,30	,'img/img-cherry.png'};
+insert into list values {'Grapes'	,70	,'img/img-grapes.png'};
+insert into list values {'Lemon'	,3	,'img/img-lemon.png'};
+insert into list values {'Orange'	,5	,'img/img-orange.png'};
+insert into list values {'Pear'	,7	,'img/img-pear.png'};
+insert into list values {'Raspberry'	,25	,'img/img-raspberry.png'};
+insert into list values {'Strawberry'	,40	,'img/img-strawberry.png'};
