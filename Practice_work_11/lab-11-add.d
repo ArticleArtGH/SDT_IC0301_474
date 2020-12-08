45bc3458976147a58570812370b4773380a758d0
Chernykh Artyom <artemchernykh07@gmail.com>
2020-12-08 20:51:32 +0300

    Pw_11

---
+++ Practice_work_11/lab-11-add.d
@@ -0,0 +1,13 @@
+lab-11-add.o: lab-11-add.c /usr/include/lua5.3/lua.h \
+ /usr/include/lua5.3/luaconf.h /usr/include/lua5.3/lualib.h \
+ /usr/include/lua5.3/lua.h /usr/include/lua5.3/lauxlib.h
+
+/usr/include/lua5.3/lua.h:
+
+/usr/include/lua5.3/luaconf.h:
+
+/usr/include/lua5.3/lualib.h:
+
+/usr/include/lua5.3/lua.h:
+
+/usr/include/lua5.3/lauxlib.h:
---
+++ Practice_work_11/lab-11.d
@@ -0,0 +1,13 @@
+lab-11.o: lab-11.c /usr/include/lua5.3/lua.h \
+ /usr/include/lua5.3/luaconf.h /usr/include/lua5.3/lualib.h \
+ /usr/include/lua5.3/lua.h /usr/include/lua5.3/lauxlib.h
+
+/usr/include/lua5.3/lua.h:
+
+/usr/include/lua5.3/luaconf.h:
+
+/usr/include/lua5.3/lualib.h:
+
+/usr/include/lua5.3/lua.h:
+
+/usr/include/lua5.3/lauxlib.h:
